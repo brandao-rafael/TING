50026778d12edac527f05cd2eda1ccca050af4b4
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-05-08 17:43:36 -0300

    feat: function to validate if exist word in 'linhas_do_arquivo'

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
     """Aqui irá sua implementaçã"""
+    result = []
+    print(instance.get_data())
+    for data in instance.get_data():
+        rows = []
+        for i, line in enumerate(data["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                rows.append({"linha": i + 1})
+        if rows:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": rows,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
