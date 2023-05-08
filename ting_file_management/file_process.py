fd214341f7f27193cbfd315c4421cb79641eb9ca
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-05-08 15:32:06 -0300

    feat: file_metadata implementation and apply some patern to stderr

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -6,16 +7,16 @@ def process(path_file, instance):
     file_name = path_file.split("/")[-1]
     for data in instance.get_data():
         if data["nome_do_arquivo"] == path_file:
-            print(f"arquivo {file_name} já processado")
+            sys.stdout.write(f"arquivo {file_name} já processado")
             return
     lines_in_file = txt_importer(path_file)
     file_data = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(lines_in_file),
-        "linhas_no_arquivo": lines_in_file,
+        "linhas_do_arquivo": lines_in_file,
     }
     instance.enqueue(file_data)
-    print(
+    sys.stdout.write(
         f"'nome_do_arquivo': '{path_file}',"
         + f"'qtd_linhas': {len(lines_in_file)},"
         + f"'linhas_do_arquivo': {lines_in_file}"
@@ -33,3 +34,8 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    try:
+        result = instance.search(position)
+        print(result)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
