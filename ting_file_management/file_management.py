8e3faf2c8b42eccda3a67caf3b4c53e09b8dfb83
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-05-08 11:57:54 -0300

    feat: function to read txt file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, mode="r", encoding="utf-8") as file:
+            return list(file.read().split("\n"))
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
