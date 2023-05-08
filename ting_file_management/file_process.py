efd5c8b1f67ba5fae8eb1c018a14481bb497e710
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-05-08 14:47:28 -0300

    refactor removing linked_list to instance be iterable

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,27 @@
+from ting_file_management.file_management import txt_importer
+
+# import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    file_name = path_file.split("/")[-1]
+    line_quantity = len(instance)
+    for i, data in enumerate(instance.get_data()):
+        if data[i] == file_name:
+            line_quantity -= 1
+    lines_in_file = txt_importer(path_file)
+    file_data = {
+        "nome_do_arquivo": file_name,
+        "qtd_linhas": line_quantity,
+        "linhas_no_arquivo": lines_in_file,
+    }
+    instance.enqueue(file_data)
+    print(
+        f"'nome_do_arquivo': '{path_file}',"
+        + f"'qtd_linhas': {len(lines_in_file)},"
+        + f"'linhas_do_arquivo': {lines_in_file}"
+    )
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,10 +1,9 @@
 from ting_file_management.abstract_queue import AbstractQueue
-from ting_file_management.linked_list import LinkedList
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__data = LinkedList()
+        self.__data = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
@@ -13,18 +12,21 @@ class Queue(AbstractQueue):
     def __str__(self):
         return f"Node(len={len(self.__data)}, value={self.__data})"
 
+    def get_data(self):
+        return self.__data
+
     def enqueue(self, value):
         """Aqui irá sua implementação"""
-        self.__data.insert_last(value)
+        self.__data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
         if len(self.__data) == 0:
             return None
-        return self.__data.remove_first().value
+        return self.__data.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
         if index < 0 or index >= len(self.__data):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.__data.get_element_at(index).value
+        return self.__data[index]
