13172a32060934fc7e4447527dd75bdd5e87a770
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-05-08 14:52:44 -0300

    feat: implementing function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,14 +6,14 @@ from ting_file_management.file_management import txt_importer
 def process(path_file, instance):
     """Aqui irá sua implementação"""
     file_name = path_file.split("/")[-1]
-    line_quantity = len(instance)
-    for i, data in enumerate(instance.get_data()):
-        if data[i] == file_name:
-            line_quantity -= 1
+    for data in instance.get_data():
+        if data["nome_do_arquivo"] == file_name:
+            print(f"arquivo {file_name} já processado")
+            return
     lines_in_file = txt_importer(path_file)
     file_data = {
         "nome_do_arquivo": file_name,
-        "qtd_linhas": line_quantity,
+        "qtd_linhas": len(lines_in_file),
         "linhas_no_arquivo": lines_in_file,
     }
     instance.enqueue(file_data)
